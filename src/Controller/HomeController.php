2f2a386daf8a8d96bed489fc80d5df4c48db5788
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-06-29 16:28:51 +0000

    Creation fonction inscription

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
@@ -25,10 +29,43 @@ class HomeController extends AbstractController
     }
 
      /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="home_logout")
      */
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * Pour s'enregistrer 
+     * @Route("/register", name="home_register")
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre compte a bien été créé !'
+            );
+
+            return $this->redirectToRoute("homepage");
+        }
+
+        return $this->render('create/index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
