a76f5e5bc90946b017f00c17403dbcd67d1dea54
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-06-30 15:17:23 +0000

    Suppression des parties login, logout et register sur ses pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,60 +14,29 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
-    public function login(AuthenticationUtils $authenticationUtils)
-    {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+    // /**
+    //  * @Route("/", name="app_login")
+    //  */
+    // public function login(AuthenticationUtils $authenticationUtils): Response
+    // {
+    //     // get the login error if there is one
+    //     $error = $authenticationUtils->getLastAuthenticationError();
+    //     // last username entered by the user
+    //     $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('home/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error
-        ]);
-    }
+    //     return $this->render('home/index.html.twig', [
+    //         'last_username' => $lastUsername,
+    //         'error' => $error,
+    //         'hasError' => $error != null
+    //     ]);
+    // }
 
-     /**
-     * @Route("/logout", name="home_logout")
-     */
-    public function logout()
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    //  /**
+    //  * @Route("/logout", name="app_logout")
+    //  */
+    // public function logout()
+    // {
+    //     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    // }
 
-    /**
-     * Pour s'enregistrer 
-     * @Route("/register", name="home_register")
-     */
-    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
-    {
-        $user = new User();
-
-        $form = $this->createForm(UserType::class, $user);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $hash = $encoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($hash);
-
-            $manager->persist($user);
-            $manager->flush();
-
-            $this->addFlash(
-                'success',
-                'Votre compte a bien été créé !'
-            );
-
-            return $this->redirectToRoute("homepage");
-        }
-
-        return $this->render('create/index.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 }
