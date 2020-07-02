19ec086f6a05ebfd1a9d0f74ba9d93ce4a9a99fc
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-02 16:26:06 +0000

    Creation de la home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,41 +2,20 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class HomeController extends AbstractController
 {
-    // /**
-    //  * @Route("/", name="app_login")
-    //  */
-    // public function login(AuthenticationUtils $authenticationUtils): Response
-    // {
-    //     // get the login error if there is one
-    //     $error = $authenticationUtils->getLastAuthenticationError();
-    //     // last username entered by the user
-    //     $lastUsername = $authenticationUtils->getLastUsername();
-
-    //     return $this->render('home/index.html.twig', [
-    //         'last_username' => $lastUsername,
-    //         'error' => $error,
-    //         'hasError' => $error != null
-    //     ]);
-    // }
-
-    //  /**
-    //  * @Route("/logout", name="app_logout")
-    //  */
-    // public function logout()
-    // {
-    //     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    // }
+    /**
+     * Page d'accueil accessible que pour les users connecté 
+     * @Route("/", name="app_home")
+     * @IsGranted("ROLE_USER")
+     */
+    public function home()
+    {
+        return $this->render('home/index.html.twig');
+    }
 
 }
