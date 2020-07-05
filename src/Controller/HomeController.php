4213618bdac4bdcdccaa526df29d5c498494aa2e
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-05 17:05:05 +0000

    Ajout de la function permettant d'afficher la page mentions legales

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -12,10 +15,24 @@ class HomeController extends AbstractController
      * Page d'accueil accessible que pour les users connecté 
      * @Route("/", name="app_home")
      * @IsGranted("ROLE_USER")
+     * 
+     * 
+     * @return Response
      */
     public function home()
     {
         return $this->render('home/index.html.twig');
+    }
+
+     /**
+     * Page des mentions légales
+     * @Route("/mentions-legales", name="app_mentions")
+     * @IsGranted("ROLE_USER")
+     * 
+     *  @return Response
+     */
+    public function mentions() {
+        return $this->render('home/mentions.html.twig');
     }
 
 }
