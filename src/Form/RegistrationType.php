74f150a548bdbdc7ac41becfeb94426d27840bb1
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-05 17:03:08 +0000

    Ajout de la checkbox validate pour obliger les nouveaux users a accepter les conditions avant inscription

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends ApplicationType
@@ -20,13 +21,17 @@ class RegistrationType extends ApplicationType
             ->add('email', EmailType::class, $this->getConfiguration("Email", ""))
             ->add('password', PasswordType::class, $this->getConfiguration("Mot de passe", ""))
             ->add('passwordConfirm',  PasswordType::class, $this->getConfiguration("Confirmation du mot de passe", ""))
-        ;
+            ->add('validate', CheckboxType::class, [
+                'invalid_message' => "Veuillez cocher la case",
+                'label' => 'En s\'inscrivant vous acceptez les conditions générales',
+                'required' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => User::class,    
         ]);
     }
 }
