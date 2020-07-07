d4c07c69aaeaaca7e3d6da7d4efe8bcda449e036
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-07 16:55:46 +0000

    Creation du champ validation des conditions generales avant inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 use Symfony\Component\Security\Core\User\UserInterface;
