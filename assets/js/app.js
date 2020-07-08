c1324e6ff9872d90f76e4cdab7042b6c2e0e2553
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-08 15:03:28 +0000

    Deplacements des scripts des pages html vers des pages specifiques js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
+const inputs = document.querySelectorAll('input');
 
+for (let i = 0; i < inputs.length; i++) {
+
+let field = inputs[i];
+
+// Si le form renvoie une erreur il faut que les labels des champs restent au dessus du texte
+if (field.value != "") {
+field.parentNode.classList.add('animation');
+}
+
+// Dès que l'user écrit le label doit passer au dessus
+field.addEventListener('input', (e) => { // console.log(e.target.value);
+if (e.target.value != "") {
+e.target.parentNode.classList.add('animation');
+} else if (e.target.value == "") {
+e.target.parentNode.classList.remove('animation');
+}
+});
+
+
+}
