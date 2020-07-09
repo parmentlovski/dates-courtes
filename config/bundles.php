f8a3306ba20d2a7f7c34d8bc779f9538b03ec19c
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-09 20:41:07 +0000

    Bundles

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Component\Dotenv\Dotenv::class => ['dev' => true, 'test' => true],
 ];
