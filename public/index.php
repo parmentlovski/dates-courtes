8dc4c1526228b1eae3cb108de9038c53b46672b7
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-07-10 09:15:05 +0000

    dot env index.php

--- public/index.php
+++ public/index.php
@@ -11,11 +11,11 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (!isset($_SERVER['APP_ENV'])) {
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+// if ($_SERVER['APP_DEBUG']) {
+//     umask(0000);
 
-    Debug::enable();
-}
+//     Debug::enable();
+// }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
