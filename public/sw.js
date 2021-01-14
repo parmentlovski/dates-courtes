833743754647da95d17e8e11a41e4bb91fa1927a
parmentlovski <parmentelot.bryan@hotmail.fr>
2021-01-14 14:13:31 +0000

    Ajout service worker

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,32 @@
 // sw.js
-// self.addEventListener('install', e => {
+self.addEventListener('install', e => {
 
-//   console.log("coucou toi");
+  console.log("coucou toi");
 
-//     e.waitUntil(
-//       // Après l'installation du service worker,
-//       // ouvre un nouveau cache
-//       caches.open('mon-cache-pwa').then(cache => {
-//         // Ajoute toutes les URLs des éléments à mettre en cache
+    e.waitUntil(
+      // Après l'installation du service worker,
+      // ouvre un nouveau cache
+      caches.open('mon-cache-pwa').then(cache => {
+        // Ajoute toutes les URLs des éléments à mettre en cache
 
-//         console.log("coucou");
+        console.log("coucou");
 
-//         return cache.addAll([
-//           '/',
-//           // '/templates/base.html.twig',
-//           // '/templates/home/index.html.twig',
-//           // '/templates/security/login.html.twig',
-//           // '/public/build/css/app.css',
-//           // '/public/build/css/app.js',
-//           // '/public/img/favicon.ico',
-//           // '/public/build/js/script.js',
-//           // '/public/build/js/dates.js',
-//           // '/public/build/js/app.js',
-//           // '/public/build/js/optional.js'
-//         ]);
-//       })
-//     );
-//    });
+        return cache.addAll([
+          '/',
+          '/templates/base.html.twig',
+          '/templates/home/index.html.twig',
+          '/templates/security/login.html.twig',
+          '/public/build/css/app.css',
+          '/public/build/css/app.js',
+          '/public/img/favicon.ico',
+          '/public/build/js/script.js',
+          '/public/build/js/dates.js',
+          '/public/build/js/app.js',
+          '/public/build/js/optional.js'
+        ]);
+      })
+    );
+   });
 
 // On install - caching the application shell
 self.addEventListener("install", function(event) {
@@ -39,31 +39,31 @@ self.addEventListener("install", function(event) {
   );
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;     // if valid response is found in cache return it
-//         } else {
-//           return fetch(event.request)     //fetch from internet
-//             .then(function(res) {
-//               return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());    //save the response for future
-//                   return res;   // return the fetched data
-//                 })
-//             })
-//             .catch(function(err) {       // fallback mechanism
-//               return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-//                 .then(function(cache) {
-//                   return cache.match('/offline.html');
-//                 });
-//             });
-//         }
-//       })
-//   );
-// });         
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
+  );
+});         
 
 // On network request
 self.addEventListener("fetch", function(event) {
